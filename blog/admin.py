d3d73ce2bd0551028a95944041c5dc23e703f212
Hakan Mollaahmetoglu <hakan.mollaahmetoglu@gmail.com>
2023-04-20 18:22:36 +0300

    Yazi Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from blog.models import KategoriModel
+from blog.models import YazilarModel
 
 admin.site.register(KategoriModel)
+
+
+class YazilarAdmin(admin.ModelAdmin):
+    search_fields=('baslik','icerik')
+    list_display=(
+        'baslik','olusturma_tarihi','duzenleme_tarihi',
+    )
+
+admin.site.register(YazilarModel,YazilarAdmin)
 
---
+++ blog/models/yazi.py
@@ -0,0 +1,23 @@
+from django.db import models
+from autoslug import AutoSlugField
+from blog.models import KategoriModel
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
+class YazilarModel(models.Model):
+    resim = models.ImageField(upload_to='yazi_resimleri')
+    baslik = models.CharField(max_length=50)
+    icerik = RichTextField()
+    olusturma_tarihi = models.DateTimeField(auto_now_add=True)
+    duzenleme_tarihi = models.DateTimeField(auto_now=True)
+    slug = AutoSlugField(populate_from='baslik',unique=True)
+    kategoriler = models.ManyToManyField(KategoriModel, related_name='yazi')
+    yazar = models.ForeignKey(User, on_delete=models.CASCADE, related_name='yazilar')
+    
+    class Meta:
+        db_table='Yazi'
+        verbose_name_plural='Yazilar'
+        verbose_name = "Yazi"
+        
+    def __str__(self):
+        return self.baslik
