e3c12a45a1d2c4917d1ce8b6a5f2345477123519
Hakan Mollaahmetoglu <hakan.mollaahmetoglu@gmail.com>
2023-04-20 18:39:29 +0300

    Yorum Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
 from blog.models import KategoriModel
 from blog.models import YazilarModel
+from blog.models import YorumModel
 
 admin.site.register(KategoriModel)
 
 
+
+
+# Yorum admin panel
+class YorumAdmin(admin.ModelAdmin):
+    search_fields=('yazan__username',)
+    list_display=(
+        'yazan','olusturma_tarihi','guncelleme_tarihi',
+    )
+
+
+
+admin.site.register(YorumModel,YorumAdmin)
+
+
+
+
+
+
+# yazılar admin panel 
 class YazilarAdmin(admin.ModelAdmin):
     search_fields=('baslik','icerik')
     list_display=(
