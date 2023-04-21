7cfbabc3c880e07b1012cd6ed6c5f82584142417
Hakan Mollaahmetoglu <hakan.mollaahmetoglu@gmail.com>
2023-04-22 02:55:01 +0300

    logou password change

--- blog/views/yorum_sil.py
+++ blog/views/yorum_sil.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from blog.models import YorumModel
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 @login_required(login_url='/')
@@ -8,6 +9,7 @@ def yorum_sil(request,id):
     yorum = get_object_or_404(YorumModel,id=id)
     if yorum.yazan == request.user or yorum.yazi.yazar == request.user:
         yorum.delete()
+        messages.success(request, 'Yorum Başarı ile Silindi')
         return redirect('detay',slug=yorum.yazi.slug)
     
     return redirect('anasayfa')
--- config/urls.py
+++ config/urls.py
@@ -11,5 +11,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('',include('blog.urls') ),
+    path('',include('blog.urls') ),
+    path('account/',include('account.urls') ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
