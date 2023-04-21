a852f516e7938815c39cf7b7c03f3cd45ee0c174
Hakan Mollaahmetoglu <hakan.mollaahmetoglu@gmail.com>
2023-04-21 15:54:08 +0300

    sayfalama

--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from blog.models import YazilarModel
+from django.core.paginator import Paginator
+
+
 
 
 
 def anasayfa(request):
+    yazilar = YazilarModel.objects.order_by('-id')
+    sayfa = request.GET.get('sayfa')
+    paginator = Paginator(yazilar,9)
+    print(paginator.get_page(sayfa))
     context={
-        'isim':'hakan Mollaahmetoğlu_Anasayfadasınız'
+        'yazilar': paginator.get_page(sayfa)
     }
     return render(request,'pages/anasayfa.html', context)
